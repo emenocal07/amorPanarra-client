379eb804e4e7fc38ccd634687a2d223bc894c1ce
Ernesto Espinoza M <emenocal07@gmail.com>
2022-03-10 12:13:13 +0100

    userDetails fixed

--- src/pages/UserDetailsPage/UserDetailsPage.jsx
+++ src/pages/UserDetailsPage/UserDetailsPage.jsx
@@ -1,11 +1,10 @@
 import { useState, useEffect, useContext } from "react"
 import usersService from "../../services/user.service"
 import UserDetails from "../../components/UserDetails/UserDetails"
-<<<<<<< HEAD
 import { AuthContext } from "../../context/Auth.context"
-=======
+import { Container } from "react-bootstrap"
 import { useParams } from "react-router-dom"
->>>>>>> 9c48c84efbbc00eab94c504b64cd8d9589dfedbb
+
 
 
 const UserDetailsPage = () => {
@@ -29,20 +28,9 @@ const UserDetailsPage = () => {
     return (
         <>
             {!isloading &&
-                <>
+                <Container>
                     <UserDetails userDetails={userDetails} />
-                    <Container>
-                        <Card.Body>
-                            <Link to='/'>
-                                <Button className='btn btn-outline-dark' variant="light" size='sm'><img style={{ width: '28px' }} src='https://res.cloudinary.com/dabjtydsw/image/upload/v1646900519/flecha2_laupdk.png' /></Button>
-                                {/* <Button style={{ width: '' }} className='btn btn-outline-warning' variant="light">Volver</Button> */}
-                            </Link>
-                            <Link to={`/perfiles/editar/${user_id}`}><Button style={{ width: '10%' }} className='btn btn-outline-success' variant="light">Editar</Button></Link>
-                            {(user?.role === 'ADMIN') &&
-                                <Button style={{ width: '10%' }} className='btn btn-outline-danger' variant="light" onClick={() => deleteProfile()}>Eliminar</Button>
-                            }
-                        </Card.Body>
-                    </Container>
+
                 </Container>
             }
         </>
